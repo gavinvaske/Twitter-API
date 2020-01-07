4e30a920296c522f468a09140066e23f821c5f09
Gavin Vaske <gavin.vaske@gmail.com>
2020-01-07 16:40:24 -0600

    pre hook config changes

--- docs/apidoc/api_project.js
+++ docs/apidoc/api_project.js
@@ -1,20 +1,17 @@
 define({
-  "*.{ts,js,json,css,md}": [
-    "npm run format",
-    "git add"
-  ],
-  "name": "Twitter API",
-  "version": "0.0.1",
-  "description": "Mock twitter API",
-  "title": "Twitter API",
-  "url": "https://github.com/gvwarrior/Twitter-API",
-  "sampleUrl": false,
-  "defaultVersion": "0.0.0",
-  "apidoc": "0.3.0",
-  "generator": {
-    "name": "apidoc",
-    "time": "2020-01-07T22:36:43.831Z",
-    "url": "http://apidocjs.com",
-    "version": "0.19.1"
-  }
-});
+  "*.{ts,js,json,css,md}": ["npm run format", "git add"],
+  name: "Twitter API",
+  version: "0.0.1",
+  description: "Mock twitter API",
+  title: "Twitter API",
+  url: "https://github.com/gvwarrior/Twitter-API",
+  sampleUrl: false,
+  defaultVersion: "0.0.0",
+  apidoc: "0.3.0",
+  generator: {
+    name: "apidoc",
+    time: "2020-01-07T22:39:24.510Z",
+    url: "http://apidocjs.com",
+    version: "0.19.1",
+  },
+})
